fdc607a97a565df5e175be46b9bfceb77a447f79
Johnny3kk <some@mail.ru>
2020-07-23 18:20:01 +0300

    HomeWork2

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -79,7 +79,7 @@ public class Main {
 //        System.out.println(mal);
 
 
-        MyArrayList<String> myArrayList = new MyArrayList<>();
+/*        MyArrayList<String> myArrayList = new MyArrayList<>();
         myArrayList.add("aaa");
         myArrayList.add("BBB");
         myArrayList.add("bbbbbbb");
@@ -93,7 +93,49 @@ public class Main {
         myArrayList.bubbleSort(Comparator.comparingInt(String::length));
 //        myArrayList.bubbleSort(Comparator.comparingInt(String::length).reversed());
 //        myArrayList.bubbleSort(Comparator.comparingInt(String::length).thenComparing(String::compareToIgnoreCase));
-        System.out.println(myArrayList);
+        System.out.println(myArrayList);*/
+
+        int mass = 100000;
+        MyArrayList<Integer> mal = new MyArrayList<>(mass);
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < mass; i++) {
+            mal.add((int) (Math.random() * mass));
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println(mal);
+        System.out.println("Total execution time of building: " + (endTime-startTime) + "ms");
+
+        MyArrayList<Integer> mal1 = new MyArrayList<>(mass);
+        for (int i = 0; i < mass; i++) {
+            mal1.add(mal.get(i));
+        }
+        long startTime1 = System.currentTimeMillis();
+        mal1.selectionSort();
+        long endTime1 = System.currentTimeMillis();
+        System.out.println(mal1);
+        System.out.println("Total execution time of Selection Sorting: " + (endTime1-startTime1) + "ms");
+
+        MyArrayList<Integer> mal2 = new MyArrayList<>(mass);
+        for (int i = 0; i < mass; i++) {
+            mal2.add(mal.get(i));
+        }
+        long startTime2 = System.currentTimeMillis();
+        mal2.insertionSort();
+        long endTime2 = System.currentTimeMillis();
+        System.out.println(mal2);
+        System.out.println("Total execution time of Insertion Sorting: " + (endTime2-startTime2) + "ms");
+
+        MyArrayList<Integer> mal3 =  new MyArrayList<>(mass);
+        for (int i = 0; i < mass; i++) {
+            mal3.add(mal.get(i));
+        }
+        long startTime3 = System.currentTimeMillis();
+        mal3.bubbleSort();
+        long endTime3 = System.currentTimeMillis();
+        System.out.println(mal3);
+        System.out.println("Total execution time of Bubble Sorting: " + (endTime3-startTime3) + "ms");
+
+
     }
 
 }
